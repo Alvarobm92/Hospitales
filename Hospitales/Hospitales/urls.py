f8b93c1be4ff099d390477d37084205d8b67d698
GuilleGC <guille_mzn@hotmail.com>
2015-04-28 18:17:02 +0200

    MVC Hospitales Funcionando

--- Hospitales/Hospitales/urls.py
+++ Hospitales/Hospitales/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hospitales/', 'iHospitales.views.hospitales', name='hospitales'),
+    url(r'^hospitales/$', 'iHospitales.views.hospitales', name='hospitales'),
     url(r'^hospitales/(?P<pk>\d+)', 'iHospitales.views.hospitales_detail' , name='hospital_detail'),
 
 
--- Hospitales/iHospitales/views.py
+++ Hospitales/iHospitales/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
+from models import Hospital
+
+
+from django.http import HttpResponse, Http404
+from django.template import Context
+from django.template.loader import get_template
+
 
 # Create your views here.
 def hospitales(request):
+    lista_hospitales = Hospital.objects.all()
+    template = get_template('hospitales.html')
+    variables = Context({
 
-
-
-
+        'hospitales':lista_hospitales
+    })
+    output = template.render(variables)
+    return HttpResponse(output)
 
 
 def hospitales_detail(request, pk):
-
+    try:
+        hospital = Hospital.objects.get(hospital_id= pk)
+        template = get_template('hospitales_detail.html')
+        variables = Context({
+            'hospital':hospital
+        })
+        output = template.render(variables)
+        return HttpResponse(output)
+    except:
+        raise Http404('User not found.')
